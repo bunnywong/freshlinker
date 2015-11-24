a0e8d50234fd7a89ee2bbe1692fd2228974b2546
BunnY <w@bunnywong.com>
2015-11-24 23:29:56 +0800

    add animate with wow dog dog

--- wp-content/themes/Freshlinker-Child/page-templates/template-single-page-of-story.php
+++ wp-content/themes/Freshlinker-Child/page-templates/template-single-page-of-story.php
@@ -14,26 +14,31 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title></title>
   <link rel="stylesheet" href="<?= get_site_url(); ?>/wp-content/themes/Freshlinker-Child/inc/css/single-page-story.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css">
   <link rel="icon" href="favicon.ico" sizes="16x16 32x32 48x48 64x64">
   <link rel="icon" href="favicon.png" sizes="192x192">
 </head>
 <body>
     <?php
-      while( have_posts() ){
-        the_post();
-        // the_content();
-      }//endwhile;
+      // while( have_posts() ){
+      //   // the_post();
+      //   // the_content();
+      // }//endwhile;
+
+      /* Ref
+        https://daneden.github.io/animate.css/
+      */
     ?>
 <div id="js-main" class="main">
-<a href="http://f.freshlinker.com/" class="header-logo" title="Freshlinker">
-            <img src="http://f.freshlinker.com/wp-content/uploads/2015/07/FreshLinker_logo-42.png" alt="Freshlinker"></a>
+<!-- <a href="http://f.freshlinker.com/" class="header-logo" title="Freshlinker">
+            <img src="http://f.freshlinker.com/wp-content/uploads/2015/07/FreshLinker_logo-42.png" alt="Freshlinker"></a> -->
   <div class="flexbox-fix-for-ie">
     <header class="section-intro vertical-center">
       <div class="container">
-        <h1 class="headline" data-sr>
+        <h1 class="headline animated slideInDown wow">
           <strong><?php wp_title( '-', true, 'right' ); ?></strong>
         </h1>
-        <nav id="js-nav" class="mainnav" data-sr="wait .25s">
+        <nav id="js-nav" class="mainnav animated slideInUp">
           <ul>
             <?php
               $custom_filed_qty = 7;
@@ -51,24 +56,28 @@
 
   <?php for($i=1; $i<=$custom_filed_qty; $i++): ?>
     <?php
-      if ($i%2==0)
+      if ($i%2==0) {
         $style = 'bglight';
-      else
+        $direction = 'slideInLeft';
+      }
+      else {
         $style = 'bgdark';
+        $direction = 'slideInRight';
+      }
     ?>
-    <section id="<?= sanitize_title(get_field('custom_title_' . $i)); ?>" class="section-about <?= $style ?>">
+    <section id="<?= sanitize_title(get_field('custom_title_' . $i)); ?>" class="section-about <?= $style ?> data-wow-duration=\"2s\" data-wow-delay=\"5s\"">
       <div class="container">
-        <h1 class="headline" data-sr="vFactor 1">
+        <h1 class="headline animated slideInDown wow">
           <?php echo the_field('custom_title_' . $i); ?>
         </h1>
-        <div class="text">
+        <div id="basic-waypoint-<?= $i; ?>" class="text animated slideInUp wow" data-wow-duration=\"1s\" data-wow-delay=\"20s\">
           <?php echo the_field('custom_content_' . $i); ?>
         </div>
       </div>
     </section>
   <?php endfor; ?>
 
-  <section id="connect" class="section-connect">
+  <section id="connect" class="section-connect wow bounceInUp center animated">
     <div class="container">
       <h1 class="headline">Register form</h1>
       <?php
@@ -79,7 +88,10 @@
 </div>
 
   <script src="<?= get_site_url(); ?>/wp-content/themes/Freshlinker-Child/inc/js/single-page-story.js" async></script>
-
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
+  <script>
+    new WOW().init();
+  </script>
 </body>
 </html>
 
